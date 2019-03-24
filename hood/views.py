f4f8fd9a23c04dc89053617f92ef87ac8e307402
abby-shabi <abbyshabi@gmail.com>
2019-03-24 18:51:01 +0300

    imports in views.py

--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.shortcuts import render,redirect,get_object_or_404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileForm,BusinessForm,ProjectForm,BusinessForm,PostForm
+from .models import Business,Profile,Project,Post
+from django.contrib.auth.models import User
+from django.http import JsonResponse
 
-from django.shortcuts import render
-
-# Create your views here.
