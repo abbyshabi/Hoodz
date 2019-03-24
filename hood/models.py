9ff58e045570e736656a6930c6078ae17320de53
abby-shabi <abbyshabi@gmail.com>
2019-03-24 18:53:01 +0300

    imports for model

--- hood/models.py
+++ hood/models.py
@@ -1,6 +1,10 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
+from tinymce.models import HTMLField
+from django.dispatch import receiver
+from django.urls import reverse
+from django.template.defaultfilters import slugify
+from django.conf import settings
 
-# Create your models here.
