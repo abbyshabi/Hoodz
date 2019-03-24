92d7e18bd704c3f711f3160ba1221cd2601b1871
abby-shabi <abbyshabi@gmail.com>
2019-03-24 19:09:59 +0300

    modified urls.py

--- hood/urls.py
+++ hood/urls.py
@@ -10,4 +10,8 @@ urlpatterns=[
     url(r'^update',views.update_profile, name= 'update'),
     url(r'^new_post',views.new_post ,name= 'new_post'),
     url(r'^add/neighbourhood',views.new_project, name= 'add_project'),
+    url(r'^new_business',views.new_business ,name= 'new_business'),
+    url(r'^project/(\d+)', views.project, name = "project"),
+    url(r'^post/(\d+)', views.post, name = "post"),
+    url(r'^new_post',views.new_post ,name= 'new_post'),
 ]
