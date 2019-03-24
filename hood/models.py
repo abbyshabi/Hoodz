07ac9bcdd5a1ebea67e83c2f34269c395923c4f4
abby-shabi <abbyshabi@gmail.com>
2019-03-24 18:53:49 +0300

    modified neighbourhood class

--- hood/models.py
+++ hood/models.py
@@ -19,4 +19,17 @@ class Neighbourhood(models.Model):
    neigh_date = models.DateTimeField(auto_now_add = True,null = True)
    location = models.CharField(max_length = 30)
    occupants = models.IntegerField(default = 0)
+
+
+class Project(models.Model):
+   """
+   This is the class we will use to create images
+   """
+   image_url = models.ImageField(upload_to = "images/")
+   name = models.CharField(max_length = 30)
+   description = models.TextField(max_length=100)
+   admin = models.ForeignKey(User,related_name='images',on_delete=models.CASCADE)
+   date = models.DateTimeField(auto_now_add = True,null = True)
+   location = models.CharField(max_length = 30)
+   occupants = models.IntegerField(default = 0)
    
