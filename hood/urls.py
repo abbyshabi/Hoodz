ea542036b41aa562ebfdfa654e7c3f3c10894ffd
abby-shabi <abbyshabi@gmail.com>
2019-03-24 19:09:27 +0300

    added urls for update etc

--- hood/urls.py
+++ hood/urls.py
@@ -7,4 +7,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$',views.index,name='home'),
     url(r'^profile',views.profile, name= 'profile'),
+    url(r'^update',views.update_profile, name= 'update'),
+    url(r'^new_post',views.new_post ,name= 'new_post'),
+    url(r'^add/neighbourhood',views.new_project, name= 'add_project'),
 ]
